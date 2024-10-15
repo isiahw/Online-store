2035a2853c67ff0108b9cb62ec1f34c90abe47d2
isiahw <raymondwigginsva@gmail.com>
2024-10-14 21:32:37 -0400

    CH 51 108 v1

---
+++ src/components/catalog.jsx
@@ -0,0 +1,12 @@
+import './styles/catalog.css';
+
+function Catalog() {
+    return (
+        <div className="catalog">
+            <h3>This is where the catalog it's going to be.</h3>
+
+        </div>
+    );
+}
+
+export default Catalog;
---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import './styles/footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <hr/>
+            <footer className="content">
+                <div>
+                    <span>Created By: Raymond wiggins</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
